87e5e586d0ca5a814bf1b9fc4acc289952b1e1f6
Jeff Montinard <68262228+jmontinard@users.noreply.github.com>
2022-05-06 12:04:33 -0400

    new commot

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,6 +1,8 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+
 import { useState } from "react"
+import AddTask from "./components/AddTask";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -24,6 +26,18 @@ function App() {
     },
     ])
 
+//Add Task 
+
+const addTask = (task) => {
+const id = Math.floor(Math.random() * 10000) + 1
+//this spread op will add in the id in the obj then copy the rest of the obj after
+const newTask = {id,...task}
+
+setTasks([...tasks,newTask])
+
+console.log(tasks);
+}
+
 // delele task
 const deleteTask = (id) => {
 setTasks(tasks.filter((task)=>  task.id !== id))
@@ -32,12 +46,13 @@ setTasks(tasks.filter((task)=>  task.id !== id))
 const toggleReminder = (id) =>{
 setTasks(tasks.map((task)=> task.id === id ? {...task, reminder: !task.reminder} : task ))
  
-  console.log('reminder');
+  // console.log('reminder');
 }
  
     return(
    <div className="container"> 
      <Header title = 'Task Tracker' />
+     <AddTask onAdd={addTask}/>
    {tasks.length > 0 ? (<Tasks tasks = {tasks} onDelete={deleteTask} onToggle={toggleReminder}  /> )
    : ('No task to show')}
    </div>
