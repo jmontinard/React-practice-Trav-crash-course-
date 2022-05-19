a928e1e12bb126845cf84e3c1029197a97064e8f
Jeff Montinard <68262228+jmontinard@users.noreply.github.com>
2022-05-18 23:05:55 -0400

    pushing commit

---
+++ task-tracker/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>Copyright &copy; 2022</p>
+        <a href='/'>Go Back </a>
+        
+         </footer>
+  )
+}
+
+export default Footer
